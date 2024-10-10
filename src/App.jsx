1c32ac81842956a2a13282223117e63a3d94ffb2
mohamad-jamil <mohamadjamil03@hotmail.com>
2024-10-11 00:24:54 +0100

    Add title and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>Test</>;
+  return (
+    <div className="d-flex justify-content-center vh-100">
+      <div className="title text-white">Password Generator</div>
+    </div>
+  );
 }
 
 export default App;
