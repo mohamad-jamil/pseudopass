a966f2b33590350abfe37fed861292621d69333c
mohamad-jamil <mohamadjamil03@hotmail.com>
2024-10-11 01:56:51 +0100

    Add Button component with placeholder for logic to regenerate passwords

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import PasswordDisplay from "./components/PasswordDisplay/PasswordDisplay";
 import Switch from "./components/Switch/Switch";
 import Slider from "./components/Slider/Slider";
+import Button from "./components/Button/Button";
 
 function App() {
+  const handleRegenerate = () => {};
+
   return (
     <div className="container d-flex flex-column vh-100 justify-content-center align-items-center">
       <div className="title text-white mb-3">Password Generator</div>
       <div className="password-display mb-2">
         <PasswordDisplay />
       </div>
-      <div className="row d-flex align-items-center">
+      <div className="row d-flex align-items-center mb-5">
         <div className="col">
           <Slider label="Length" />
         </div>
@@ -22,6 +25,11 @@ function App() {
         </div>
         <div className="col">
           <Switch label="Symbols?" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Button label="Regenerate" handleClick={handleRegenerate} />
         </div>
       </div>
     </div>
